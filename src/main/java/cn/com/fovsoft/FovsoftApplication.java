55053eae50ada070aeae1b11e592b82ce43f3793
tpc4289 <tangpengcheng1989@126.com>
2019-11-18 18:12:25 +0800

    完善收入信息表单

--- src/main/java/cn/com/fovsoft/FovsoftApplication.java
+++ src/main/java/cn/com/fovsoft/FovsoftApplication.java
@@ -2,6 +2,8 @@ package cn.com.fovsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
@@ -13,3 +15,16 @@ public class FovsoftApplication {
     }
 
 }
+
+//@SpringBootApplication
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class FovsoftApplication extends SpringBootServletInitializer{
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(FovsoftApplication.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(FovsoftApplication.class,args);
+//    }
+//}
