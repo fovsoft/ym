fed77ba348dc07cccef1af873be8b2182783bdf6
tpc4289 <tangpengcheng1989@126.com>
2019-12-16 23:45:24 +0800

    调整收入信息20191216

--- src/main/java/cn/com/fovsoft/FovsoftApplication.java
+++ src/main/java/cn/com/fovsoft/FovsoftApplication.java
@@ -6,25 +6,25 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-//@SpringBootApplication
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//public class FovsoftApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(FovsoftApplication.class, args);
-//    }
-//
-//}
-
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class FovsoftApplication extends SpringBootServletInitializer{
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(FovsoftApplication.class);
-    }
+public class FovsoftApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FovsoftApplication.class,args);
+        SpringApplication.run(FovsoftApplication.class, args);
     }
+
 }
+
+//@SpringBootApplication
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class FovsoftApplication extends SpringBootServletInitializer{
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(FovsoftApplication.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(FovsoftApplication.class,args);
+//    }
+//}
